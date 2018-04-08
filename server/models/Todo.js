269d7b76ffec9f53aaa895a8bd693b8515683a56
barestu <fheartnet@gmail.com>
2018-04-08 10:00:05 +0700

    Todos CRUD

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -2,13 +2,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let todoSchema = new Schema ({
-  name: {type: String, required: true},
-  done: {type: String, default: false},
-  due_date: {type: Date, required: true}
+  user: {
+    type: Schema.Types.ObjectId, ref: 'User'
+  },
+  task: {
+    type: String, 
+    required: [true, 'required!']
+  },
+  done: {
+    type: Boolean, 
+    default: false
+  },
+  due_date: {
+    type: Date, 
+    required: true
+  }
 },{
   timestamps: true
 })
 
 let Todo = mongoose.model('Todo', todoSchema)
+
+todoSchema.pre('findOneAndUpdate', function() {
+  console.log('BEFORE UPDATE')
+})
 
 module.exports = Todo
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,10 +1,12 @@
 const { loginAuth, adminAuth } = require('../middleware/auth')
-const { showTodo, addTodo, updateTodo, deleteTodo } = require('../controllers/todos_controller')
+const { findMine, findAll, findOne, add, update, remove } = require('../controllers/todos_controller')
 const router = require('express').Router()
 
-router.get('/', loginAuth, showTodo)
-      .post('/add', adminAuth, addTodo)
-      .put('/update', loginAuth, updateTodo)
-      .delete('/delete', loginAuth, deleteTodo)
+router.get('/', loginAuth, findMine)
+      .get('/all', adminAuth, findAll)
+      .get('/search/:id', adminAuth, findOne)
+      .post('/add', loginAuth, add)
+      .put('/update/:id', loginAuth, update)
+      .delete('/delete/:id', loginAuth, remove)
 
 module.exports = router
