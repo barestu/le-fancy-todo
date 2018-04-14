3b1775ce1876efa860c7b239f1f91f070948da4b
barestu <fheartnet@gmail.com>
2018-04-15 01:13:21 +0700

    Login auth for main page

--- client/src/js/vue-app.js
+++ client/src/js/vue-app.js
@@ -1,11 +1,29 @@
 let vue = new Vue({
   el: '#app',
   data: {
+    isLogin: false,
     taskName: '',
     dueDate: '',
     listTodos: [],
   },
   methods: {
+    checkAuth: function() {
+      let token = localStorage.getItem('token')
+
+      if (token) {
+        this.isLogin = true
+      } else {
+        this.isLogin = false
+        window.location.href = 'https://4a55af47.ngrok.io/'
+      }
+    },
+
+    logout: function() {
+      console.log('log out clicked')
+      localStorage.removeItem('token')
+      window.location.href = 'https://4a55af47.ngrok.io/'
+    },
+
     addTask: function() {
       let token = localStorage.getItem('token')
       let newTask = {
@@ -22,6 +40,16 @@ let vue = new Vue({
       .catch(err => {
         console.log('add failed', err)
       })
+    }
+  },
+  
+  beforeMount() {
+    let location = window.location.href
+    let indexPage = 'https://4a55af47.ngrok.io/'
+    let registerPage = 'https://4a55af47.ngrok.io/register.html'
+
+    if (location !== indexPage && location !== registerPage) {
+      this.checkAuth()
     }
   }
 })
