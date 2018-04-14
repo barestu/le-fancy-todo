daabbf79c8305d57228c9dcdf6ff3b07fec9c2a4
barestu <fheartnet@gmail.com>
2018-04-14 16:36:30 +0700

    test

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 4000
+const db_username = process.env.DB_USERNAME
+const db_password = process.env.DB_PASSWORD
 
 const indexRoutes = require('./routes/index')
 const userRoutes = require('./routes/users')
@@ -13,7 +15,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 
-mongoose.connect(`mongodb://localhost/todo-fancy`)
+mongoose.connect(`mongodb://${db_username}:${db_password}@ds237389.mlab.com:37389/todo-fancy`)
+// mongoose.connect(`mongodb://localhost/todo-fancy`)
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Connection error:'))
 db.once('open', function() {
